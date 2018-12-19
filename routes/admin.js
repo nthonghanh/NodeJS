859c5da73cb440e4de943fb54122e7d3e28120d7
Nguyen Thi Hong Hanh <nthonghanh@tma.com.vn>
2018-12-19 15:48:24 +0700

    Data store

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,16 @@
 import express from 'express';
-import { getAddProduct, postAddProduct, getProducts } from '../controllers/admin';
+import { getAddProduct, postAddProduct, getProducts, getEditProduct, postEditProduct, postDeleteProduct } from '../controllers/admin';
 
 const router = express.Router();
 
 /* GET method */
 router.get('/add-product', getAddProduct);
 router.get('/products', getProducts);
+router.get('/edit-product/:productId', getEditProduct);
 
 /* POST method */
 router.post('/add-product', postAddProduct);
+router.post('/edit-product', postEditProduct);
+router.post('/delete-product', postDeleteProduct);
 
 module.exports.routes = router;
